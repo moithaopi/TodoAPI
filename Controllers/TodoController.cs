c1cdecc3402c8402210d19b29bb328603211b006
Moithaopi Monwela <mmonwela@botswanapost.co.bw>
2020-11-11 08:08:49 +0200

    add Todo Controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,31 +1,110 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using TodoAPI.Models;
 
 namespace TodoAPI.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class TodoController : ControllerBase
     {
         private readonly TodoContext _context;
+
         public TodoController(TodoContext context)
         {
             _context = context;
         }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodo()
+        {
+            return await _context.Todo.ToListAsync();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodo(int id)
+        {
+            var todo = await _context.Todo.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return todo;
+        }
+
+        // PUT: api/Todo/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodo(int id, Todo todo)
+        {
+            if (id != todo.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
         // POST: api/Todo
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<Todo>> PostTodo(Todo todo)
         {
             _context.Todo.Add(todo);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetTodo", new { id = todo.id }, todo);
-            //return CreatedAtAction(nameof(GetTodo), new { id = todo.id }, todo);
+            //return CreatedAtAction("GetTodo", new { id = todo.id }, todo);
+            return CreatedAtAction(nameof(GetTodo), new { id = todo.id }, todo);
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Todo>> DeleteTodo(int id)
+        {
+            var todo = await _context.Todo.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todo.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todo.Any(e => e.id == id);
         }
     }
 }
